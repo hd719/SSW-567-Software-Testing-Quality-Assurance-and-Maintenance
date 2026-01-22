edd8f6dcb13d76f8eda13dfb88c392cfbf631997
Hamel Desai <hameldesai93@gmail.com>
2026-01-22 11:13:11 -0500

    Assignment-1-Tools-Setup

---
+++ assignment-1-tools-setup/main.py
@@ -0,0 +1,8 @@
+def greeting() -> str:
+    return "Hello from ssw-567-software-testing-quality-assurance-and-maintenance!"
+
+def main() -> None:
+    print(greeting())
+
+if __name__ == "__main__":
+    main()
